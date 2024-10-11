d0484446ae1aa83732e16c3ff26255e23ff6d1bf
aryanguptacsvtu <148983503+aryanguptacsvtu@users.noreply.github.com>
2024-10-11 12:09:16 +0530

    Input_Output

---
+++ Input_Output.cpp
@@ -0,0 +1,17 @@
+# include<iostream>
+using namespace std;
+
+
+int main()
+{
+    int num1,num2;
+    cout<<"Enter the value of Num1:"; // "<<" is called Insertion operator.
+    cin>>num1;                        // ">>" is called Extraction operator.
+                                 
+    cout<<"Enter the value of Num2:\n";
+    cin>>num2;
+    
+    cout<<"The sum is:"<<num1+num2;
+    
+    return 0;
+}
