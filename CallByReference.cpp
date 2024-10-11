1b2c43b2b7ecb59760df96e094ba6c56f00ec5ad
aryanguptacsvtu <148983503+aryanguptacsvtu@users.noreply.github.com>
2024-10-11 12:01:50 +0530

    CallByReference

---
+++ CallByReference.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+void swap(int *a, int *b)
+{
+    int temp;
+    temp = *a;
+    *a = *b;
+    *b = temp;
+}                  // Call by Reference has 'effect' on Actual arguments.
+
+int main()
+{
+    int a = 10, b = 50;
+
+    cout << "Value of a:" << a << " and value of b:" << b << " before swapping." << endl;
+    swap(&a, &b);
+    cout << "Value of a:" << a << " and value of b:" << b << " after swapping." << endl;
+    return 0;
+}
