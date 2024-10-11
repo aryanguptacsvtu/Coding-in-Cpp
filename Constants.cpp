cfeacbde5276db8e8305c2ff1b314e4b992a5a79
aryanguptacsvtu <148983503+aryanguptacsvtu@users.noreply.github.com>
2024-10-11 12:03:30 +0530

    Constants

---
+++ Constants.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+   const int a = 324;
+   const float b= 45.23;
+
+ cout<<"The value of a is "<<a<<" which cannot be changed."<<endl;
+ cout<<"The value of b is "<<b<<" which cannot be changed."<<endl;
+
+    return 0;
+}
