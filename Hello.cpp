41c90277b52f1564dd83e9a44e9c5189f56bca33
aryanguptacsvtu <148983503+aryanguptacsvtu@users.noreply.github.com>
2024-10-11 12:07:20 +0530

    Hello

---
+++ Hello.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+// This is a single line comment.
+/*This 
+is a
+multi line 
+comment.*/
+
+int main()
+{
+    cout<<"Hello World!!";
+    
+    return 0;
+   
+}
